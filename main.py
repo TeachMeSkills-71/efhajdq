37b09ff839cd0d3b0f8e6ff1334953653a678493
nazar <nazar.konovalov123@mail.ru>
2022-02-08 14:55:10 +0300

    checking for prime numbers

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('dsfsa')
+def rec(x):
+    a = int(input('ведите число'))
+    flag = True
+    b = 2
+    while b * b <= a:
+        if a % b == 0:
+            flag = False
+            break
+    if flag:
+        print(f'{a} - число простое ')
+    else:
+        print(f'{a} - составное число')
+    print(x)
+    rec(x)
+rec(1)
+
