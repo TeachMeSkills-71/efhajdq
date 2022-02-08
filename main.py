024c7de3a739e79699ee92228b96e3b43a6ae702
nazar <nazar.konovalov123@mail.ru>
2022-02-08 14:04:05 +0300

    While True

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('aege')
+while True:
+    a = int(input('insert the number a'))
+    b = int(input('insert the number b='))
+    print('1 - fold a+b')
+    print('2 - subtract a-b')
+    print('3 - multiplication a*b')
+    print('4 - division a/b')
+    operation = int(input('choose an operation'))
+
+    if operation == 1:
+        print('soma a+b=', a + b)
+
+    if operation == 2:
+        print('difference a-b=', a - b)
+
+    if operation == 3:
+        print('sets a*b=', a * b)
+
+    if operation == 4:
+        print('del a/b=', a / b)
