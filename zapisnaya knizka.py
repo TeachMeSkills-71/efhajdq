da35f370ee9b6e90f5fb6e293bd5c61d4d4b1dc1
nazar <nazar.konovalov123@mail.ru>
2022-02-09 10:52:40 +0300

    checking for prime numbers and validnost and zapisnaya knizka

--- zapisnaya knizka.py
+++ zapisnaya knizka.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 def append_work(data):
     inp = input("Введите заплонированные дела через запятую: ").split(",")
     csv_file = pd.read_csv("note.csv")
@@ -20,23 +18,3 @@ data = {"Дела":[], "Статус":[], "Время":[]}
 
 append_work(data)
 
-while True:
-    what = input("1-добавить новые дела, 2-поменять статус дела, 3-стоп: ")
-    if what == "1":
-        append_work(data)
-
-    elif what == "2":
-        sts = input("Введите индекс и статус дела через пробел: ").split(" ")
-        csv_file = pd.read_csv("note.csv")
-
-        df = dict(pd.concat([csv_file, pd.DataFrame(data)], ignore_index=True))
-
-        status = list(df['Статус'])
-        status[int(sts[0])] = sts[1]
-        df['Статус'] = status
-
-        pd.DataFrame(df).to_csv("note.csv", index=None)
-        print(pd.DataFrame(df))
-
-    elif what == "3":
-        break
